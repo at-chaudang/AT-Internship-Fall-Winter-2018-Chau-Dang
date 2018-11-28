c4d14559ad31a37f4fc65f9b3a940209616c1082
at-chau dang <chau.dang@asiantech.vn>
2018-11-28 17:58:08 +0700

    Js-ex version 2.0

--- js/database.js
+++ js/database.js
@@ -1,41 +1,41 @@
 let db_products = [
   {
-    idP: 0,
+    idP: 10,
     image: "images/pro-clothes1.jpg",
     nameP: "Black Sweater",
     description: "Áo Len đen",
     price: 100
   },
   {
-    idP: 1,
+    idP: 11,
     image: "images/pro-clothes2.jpg",
     nameP: "Brown Sweater",
     description: "Áo Len có cổ nâu",
     price: 150
   },
   {
-    idP: 2,
+    idP: 12,
     image: "images/pro-clothes3.jpg",
     nameP: "Pink Dress",
     description: "Váy hồng",
     price: 300
   },
   {
-    idP: 3,
+    idP: 13,
     image: "images/pro-clothes4.jpg",
     nameP: "AX Dress",
     description: "Váy sọc ngang",
     price: 400
   },
   {
-    idP: 4,
+    idP: 14,
     image: "images/pro-clothes5.jpg",
     nameP: "Three color T-shirt",
     description: "Áo thun 3 màu",
     price: 500
   },
   {
-    idP: 5,
+    idP: 15,
     image: "images/pro-clothes6.jpg",
     nameP: "DVC T-shirt",
     description: "Áo thun DVC",
--- js/js-cart.js
+++ js/js-cart.js
@@ -5,35 +5,67 @@ let btnRemove = document.getElementsByClassName('ic-remove');
 let tableBody = document.getElementById('listCart');
 let child;
 let sum = 0;
+let tableTemp = [];
+
 total.innerHTML = productIds.length;
+
+function renderView() {
+  getTableTemp();
+  document.getElementById('listCart').innerHTML = showListProducts();
+  total.innerHTML = productIds.length;
+  removeCart();
+}
+
 function showListProducts() {
-  for (let i = 0; i < productIds.length; i++) {
-    listCart[i] = '<tr data-id="' + productIds[i] + '" id="product-' + i + '"><td class="product-td">' +
-    '<a><img class="product-image" src="' + db_products[productIds[i]].image + '"></a>' +
-    '<div class="float-right"><h3 class="product-name">' + db_products[productIds[i]].nameP + '</h3>' +
-    '<p class="product-description">' + db_products[productIds[i]].description + '</p></div></td>' +
+  sum = 0;
+  listCart.length = 0;
+  for (let i = 0; i < tableTemp.length; i++) {
+    listCart[i] = '<tr data-id="' + tableTemp[i].idP + '"><td class="product-td">' +
+    '<a><img class="product-image" src="' + tableTemp[i].image + '"></a>' +
+    '<div class="float-right"><h3 class="product-name">' + tableTemp[i].nameP + '</h3>' +
+    '<p class="product-description">' + tableTemp[i].description + '</p></div></td>' +
     '<td><span>' + 1 + '</span></td>' +
-    '<td><span>$' + db_products[productIds[i]].price + '</span></td>' +
+    '<td><span>$' + tableTemp[i].price + '</span></td>' +
     '<td><a><img src="images/ic_remove.png" class="ic-remove"></a></td></tr>';
-    sum += db_products[productIds[i]].price;
+    sum += tableTemp[i].price;
   }
   return listCart.join('') + '<tr><td><b>Total: </b></td><td colspan="3"><span id="total">$' + sum + '</span></td></tr>';
 }
 
-document.getElementById('listCart').innerHTML = showListProducts();
+function getTableTemp() {
+  tableTemp.length = 0;
+  for (let i = 0; i < db_products.length; i++) {
+    for (let j = 0; j < productIds.length; j++) {
+      if (+db_products[i].idP === +productIds[j]) {
+        tableTemp.push(db_products[i]);
+      }
+    }
+  }
+  console.log(tableTemp);
+  // tableTemp = productIds.map(u => db_products.filter(v => v.idP === u));
+};
 
-(function removeCart() {
+function removeCart() {
   for (let i = 0; i < btnRemove.length; i++) {
-    btnRemove[i].addEventListener('click', function(r) {
+    btnRemove[i].addEventListener('click', function() {
       if (confirm("Remove out Cart!")) {
-        child = document.getElementById('product-' + i);
+        // child = document.getElementById('product-' + i);
+        // check again data ? ko check lai van ok ma? thoi thi khoi check
+        productIds = sessionStorage.getItem('productIds') ? JSON.parse(sessionStorage.getItem('productIds')) : [];
+        child = this.parentElement.parentElement.parentElement;
+        console.log(child);
         tableBody.removeChild(child);
-        sum -= +db_products[productIds[i]].price;
-        document.getElementById('total').innerHTML = sum;
-        productIds.splice(i, 1);
+        var idP = +child.getAttribute('data-id');
+        productIds = productIds.filter(function(value){
+          return idP !== +value ;
+        });
+        // productIds.splice(i, 1);
+        console.log(productIds);
         sessionStorage.setItem('productIds', JSON.stringify(productIds));       
-        total.innerHTML = productIds.length;
+        renderView();
       };
     });
   }
-})();
+};
+
+renderView();
